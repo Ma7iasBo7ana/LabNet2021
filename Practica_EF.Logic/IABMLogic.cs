38d87c977cbafff43a0946a0b85b268a12a7250e
Ma7iasBo7ana <70921047+Ma7iasBo7ana@users.noreply.github.com>
2021-04-27 22:28:21 -0300

    Subida de Practica EF

---
+++ Practica_EF.Logic/IABMLogic.cs
@@ -0,0 +1,19 @@
+﻿using Practica_EF.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica_EF.Logic
+{
+    interface IABMLogic<T>
+    {
+        List<T> GetAll();
+
+        //void add(T nuevoTerritorio);
+        //void delete(int id);
+        void Update(T territorio);
+
+    }
+}
---
+++ Practica_EF.Logic/ProductsLogic.cs
@@ -0,0 +1,42 @@
+﻿using Practica_EF.Data;
+using Practica_EF.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica_EF.Logic
+{
+    public class ProductsLogic:BaseLogic,IABMLogic<Products>
+    {
+        public List<Products> GetAll()
+        {
+            return context.Products.Where(p => p.CategoryID == 1).ToList();
+        }
+        public int add(Products nuevoProducto)
+        {
+            context.Products.Add(nuevoProducto);
+
+            context.SaveChanges();
+            return nuevoProducto.ProductID;
+        }
+        public void Delete(int id)
+        {
+            var eliminarProducto = context.Products.Find(id);
+
+            context.Products.Remove(eliminarProducto);
+
+            context.SaveChanges();
+        }
+
+        public void Update(Products producto)
+        {
+            var ActualizarProducto = context.Products.Find(producto.ProductID);
+
+            ActualizarProducto.ProductName = producto.ProductName;
+
+            context.SaveChanges();
+        }
+    }
+}
